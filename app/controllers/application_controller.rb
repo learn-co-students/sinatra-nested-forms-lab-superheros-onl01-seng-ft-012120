3779c9a87db761dc25d735c420ae8779d7a7956e
reddevilcero <reddevil_cero@hotmail.com>
2020-02-26 10:06:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
+    get '/' do
+      erb :index
+    end
+
+    post '/team' do
+      @p= params['team']
+      erb :team
+    end
 end
